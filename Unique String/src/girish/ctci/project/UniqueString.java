bb2f1c36a098cbe3847597b0031792496a07b65b
Girish Raman <rgirish1994@gmail.com>
2015-09-26 18:48:39 -0700

    Done with Unique String

--- Unique String/src/girish/ctci/project/UniqueString.java
+++ Unique String/src/girish/ctci/project/UniqueString.java
@@ -1,5 +1,30 @@
+/**
+ * @author Girish Raman
+ * Checks if a String has all unique characters, without using any data structures!
+ */
 package girish.ctci.project;
 
 public class UniqueString {
+
+	public static void main(String[] a) {
+
+		String s = "asdqwea";
+		boolean flag = false;
+
+		for (int i = s.length() - 1; i >= 0; --i) {
+			for (int j = i - 1; j >= 0; --j) {
+				if (s.charAt(j) == s.charAt(i)) {
+					flag = true;
+					System.out.println("String has duplicate characters!");
+					break;
+				}
+			}
+			if (flag)
+				break;
+		}
+		if (!flag)
+			System.out.println("String doesn't have duplicate characters!");
+
+	}
 
 }

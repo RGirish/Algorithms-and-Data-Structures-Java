5cba695e5bca74b6332e7c60481da6c3b1dbf294
Girish Raman <rgirish1994@gmail.com>
2015-10-16 23:14:08 -0700

    Done with is it a power of 2?

---
+++ IsItAPowerOfTwo/src/girish/ctci/project/IsITAPowerOfTwo.java
@@ -0,0 +1,11 @@
+package girish.ctci.project;
+
+public class IsITAPowerOfTwo {
+
+	/*
+	 * Need I say more? :D
+	 */
+	public static void main(String[] args) {
+		System.out.println(Integer.bitCount(1) == 1 ? "Power of 2!" : "Not a power of 2!");
+	}
+}

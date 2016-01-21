aeb79a0cae0faa3974fccca6547d91f39738fd9a
Girish Raman <rgirish1994@gmail.com>
2016-01-21 12:42:24 -0700

    NumberInWords works for all 12 digit numbers

--- NumberInWords/src/NumberInWords.java
+++ NumberInWords/src/NumberInWords.java
@@ -1,29 +1,43 @@
 
 public class NumberInWords {
 
-	static String[] numbers = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-			"ten", };
+	static String[] numbers = { "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", };
 	static String[] tensPosition = { "", "", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty",
 			"ninety" };
 	static String[] positions = { "", "", "hundred", "thousand" };
 
 	public static void main(String[] args) {
-		System.out.println(numberInWords(1435));
+		System.out.println(numberInWords(100200000333l));
 	}
 
-	static String numberInWords(int n) {
+	static String numberInWords(long n) {
 		String N = String.valueOf(n);
 		StringBuilder builder = new StringBuilder();
 
+		if (N.length() >= 10 && N.length() <= 12) {
+			String s = numberInWords(Integer.parseInt(N.substring(0, N.length() - 9)));
+			if (!s.trim().equals(""))
+				builder.append(s + " billion ");
+			N = N.substring(N.length() - 9, N.length());
+		}
+		if (N.length() >= 7 && N.length() <= 9) {
+			String s = numberInWords(Integer.parseInt(N.substring(0, N.length() - 6)));
+			if (!s.trim().equals(""))
+				builder.append(s + " million ");
+			N = N.substring(N.length() - 6, N.length());
+		}
 		if (N.length() >= 5 && N.length() <= 6) {
-			builder.append(numberInWords(Integer.parseInt(N.substring(0, N.length() - 3))));
+			String s = numberInWords(Integer.parseInt(N.substring(0, N.length() - 3)));
+			if (!s.trim().equals(""))
+				builder.append(s + " thousand ");
+			N = N.substring(N.length() - 3, N.length());
 		}
 		for (int i = 0; i < N.length(); ++i) {
 			int k = Integer.parseInt(String.valueOf(N.charAt(i)));
 			if (i + 2 == N.length()) {
-				if (!builder.toString().equals(""))
-					builder.append("and ");
 				int k2 = Integer.parseInt(String.valueOf(N.charAt(i + 1)));
+				if (!builder.toString().equals("") && !(k == 0 && k2 == 0))
+					builder.append("and ");
 				if (k == 1) {
 					if (k2 == 0) {
 						builder.append("ten");
@@ -46,10 +60,11 @@ public class NumberInWords {
 			} else if (i + 1 == N.length()) {
 				builder.append(numbers[k] + " ");
 			} else {
-				builder.append(numbers[k] + " " + positions[N.length() - i - 1] + " ");
+				if (k != 0)
+					builder.append(numbers[k] + " " + positions[N.length() - i - 1] + " ");
 			}
 		}
-		return builder.toString();
+		return builder.toString().replaceAll(" +", " ").trim();
 	}
 
 }

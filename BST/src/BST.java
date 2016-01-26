654ca400850245150e7a203dc1963954e7161f6c
Girish Raman <rgirish1994@gmail.com>
2016-01-26 11:13:43 -0700

    Added bfs to bst

--- BST/src/BST.java
+++ BST/src/BST.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BST {
 
 	Node root;
@@ -38,33 +41,52 @@ public class BST {
 		} else {
 			if (node.left == null && node.right == null) {
 				node = null;
-			} else if(node.left == null && node.right != null) {
+			} else if (node.left == null && node.right != null) {
 				node = node.right;
-			} else if(node.left != null && node.right == null) {
+			} else if (node.left != null && node.right == null) {
 				node = node.left;
 			} else {
-				
+
 			}
 		}
 	}
-	
+
 	void printInOrder(Node node) {
-		if(node.left != null) printInOrder(node.left);
+		if (node.left != null)
+			printInOrder(node.left);
 		System.out.print(node.n + " ");
-		if(node.right != null) printInOrder(node.right);
+		if (node.right != null)
+			printInOrder(node.right);
 	}
-	
-	void printPostOrder(Node node) {
-		if(node.left != null) printPostOrder(node.left);
-		if(node.right != null) printPostOrder(node.right);
-		System.out.print(node.n + " ");
-		
-	}
-	
+
 	void printPreOrder(Node node) {
 		System.out.print(node.n + " ");
-		if(node.left != null) printPreOrder(node.left);
-		if(node.right != null) printPreOrder(node.right);
+		if (node.left != null)
+			printPreOrder(node.left);
+		if (node.right != null)
+			printPreOrder(node.right);
+	}
+
+	void printPostOrder(Node node) {
+		if (node.left != null)
+			printPostOrder(node.left);
+		if (node.right != null)
+			printPostOrder(node.right);
+		System.out.print(node.n + " ");
+
+	}
+
+	void printLevelOrder(Node node) {
+		Queue<Node> queue = new LinkedList<Node>();
+		queue.add(node);
+		while (!queue.isEmpty()) {
+			Node temp = queue.remove();
+			System.out.print(temp.n + " ");
+			if (temp.left != null)
+				queue.add(temp.left);
+			if (temp.right != null)
+				queue.add(temp.right);
+		}
 	}
 
 }
--- BST/src/Main.java
+++ BST/src/Main.java
@@ -2,16 +2,14 @@
 public class Main {
 
 	public static void main(String[] args) {
-		BST bst = new BST(10);
+		BST bst = new BST(40);
 		bst.insert(bst.root, 20);
-		bst.insert(bst.root, 5);
-		bst.insert(bst.root, 12);
-		bst.insert(bst.root, 8);
-		bst.printInOrder(bst.root);
-		System.out.println("");
-		bst.printPreOrder(bst.root);
-		System.out.println("");
-		bst.printPostOrder(bst.root);
+		bst.insert(bst.root, 60);
+		bst.insert(bst.root, 10);
+		bst.insert(bst.root, 30);
+		bst.insert(bst.root, 50);
+		bst.insert(bst.root, 70);
+		bst.printLevelOrder(bst.root);
 	}
 
 }
